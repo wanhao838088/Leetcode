456c205d606c42ab4e81420826e89174ddf8a1ec
wanhao <838088516@qq.com>
2019-05-30 20:40:43 +0800

    104. 二叉树的最大深度

--- src/com/leetcode/maximumDepthOfBinaryTree/TestBinaryTree.java
+++ src/com/leetcode/maximumDepthOfBinaryTree/TestBinaryTree.java
@@ -5,7 +5,21 @@ package com.leetcode.maximumDepthOfBinaryTree;
  * Created by LiuLiHao on 2019/5/30 0030 下午 08:18
  * @author : LiuLiHao
  * 描述：
+ *给定一个二叉树，找出其最大深度。
  *
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最大深度 3 。
  */
 public class TestBinaryTree {
 

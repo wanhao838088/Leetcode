85ddf9a198674cae5a0ad997740894b9b3749110
wanhao <838088516@qq.com>
2019-06-21 17:41:42 +0800

    227. 基本计算器 II

---
+++ src/com/leetcode/twoThreeHundred/basicCalculatorii/TestBasicCalculatorIi.java
@@ -0,0 +1,164 @@
+package com.leetcode.twoThreeHundred.basicCalculatorii;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by LiuLiHao on 2019/6/21 0021 下午 05:41
+ * @author : LiuLiHao
+ * 描述：
+ */
+public class TestBasicCalculatorIi {
+
+
+    public static void main(String[] args) {
+        int i = '/';
+        System.out.println(i);
+        String exp = "3/2";
+        int calculate = calculate(exp);
+        System.out.println(calculate);
+
+    }
+
+    public static int calculate(String s) {
+        //去空格
+        s = s.replaceAll(" ","");
+        //先转list
+        List<String> list = getList(s);
+        //再转后缀
+        List<String> suffix = getSuffix(list);
+        //求值
+        int calc = calc(suffix);
+        return calc;
+    }
+
+    private static int calc(List<String> exps){
+        if (exps.size()==0){
+            return 0;
+        }else if (exps.size()==1){
+            return Integer.parseInt(exps.get(0));
+        }
+        Stack<String> stack = new Stack<>();
+
+        int result = 0,num1=0,num2=0;
+
+        for (int i = 0; i < exps.size(); i++) {
+            String token = exps.get(i);
+
+            try {
+                int parseInt = Integer.parseInt(token);
+                stack.push(token);
+            } catch (NumberFormatException e) {
+                //是运算符
+                num2 = Integer.parseInt(stack.pop());
+                num1 = Integer.parseInt(stack.pop());
+
+                switch (token){
+                    case "+":
+                        result = num1+num2;
+                        break;
+                    case "-":
+                        result = num1-num2;
+                        break;
+                    case "*":
+                        result = num1*num2;
+                        break;
+                    case "/":
+                        result = (int) (1.0*num1/num2);
+                        break;
+                    default:break;
+                }
+                stack.push(result+"");
+            }
+        }
+        return result;
+    }
+    /**
+     * 中缀转后缀表达式
+     * @param org
+     * @return
+     */
+    private static List<String> getSuffix(List<String> org){
+        Stack<String> s1 = new Stack<>();
+        List<String> s2 = new ArrayList<>();
+        for (int i = 0; i < org.size(); i++) {
+            String s = org.get(i);
+            if (s.matches("\\d+")){
+                //数字直接入
+                s2.add(s);
+            }else {
+                if (s.equals("(")){
+                    //左括号直接入
+                    s1.push(s);
+                }else if(s.equals(")")){
+                    //消除括号
+                    while (!s1.peek().equals("(")){
+                        s2.add(s1.pop());
+                    }
+                    //消除
+                    s1.pop();
+                }else {
+                    //当前运算符小于等于栈顶的 入栈
+                    while (s1.size()!=0 && opertatorPrior(s)<= opertatorPrior(s1.peek())){
+                        s2.add(s1.pop());
+                    }
+                    s1.push(s);
+                }
+            }
+        }
+        while (!s1.isEmpty()){
+            s2.add(s1.pop());
+        }
+        return s2;
+    }
+
+    private static int opertatorPrior(String s){
+        //优先级判断
+        switch (s){
+            case "+":
+                return 1;
+            case "-":
+                return 1;
+            case "*":
+                return 2;
+            case "/":
+                return 2;
+            default:return 0;
+        }
+    }
+    private static List<String> getList(String s){
+        List<String> result = new ArrayList<>();
+
+        int index = 0;
+        String temp = "";
+        while (index<s.length()){
+            char c = s.charAt(index);
+
+            if (c>47 && c<58){
+                temp = "";
+                while (c>47 && c<58){
+                    temp+=c;
+                    index++;
+                    if (index<s.length()){
+                        c = s.charAt(index);
+                        if (c<48 || c>58){
+                            index--;
+                            break;
+                        }
+                    }else {
+                        break;
+                    }
+                }
+                result.add(temp);
+            }else {
+                //是操作符
+                result.add(s.charAt(index)+"");
+            }
+            index++;
+        }
+        return result;
+    }
+
+
+}

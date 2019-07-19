fc1332ee17fee304989dd95cdd292c33368b1430
wanhao <838088516@qq.com>
2019-07-19 15:02:07 +0800

    2. 两数相加

---
+++ src/com/leetcode/zeroToTwoHundred/addTwoNumbers/TestAdd.java
@@ -0,0 +1,62 @@
+package com.leetcode.zeroToTwoHundred.addTwoNumbers;
+
+/**
+ * Created by LiuLiHao on 2019/7/19 0019 下午 02:34
+ * @author : LiuLiHao
+ * 描述：
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ *
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ *
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ */
+public class TestAdd {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        ListNode l2 = new ListNode(4);
+        ListNode l3 = new ListNode(3);
+
+        l1.next = l2;
+        l2.next = l3;
+
+        ListNode n1 = new ListNode(5);
+        ListNode n2 = new ListNode(6);
+        ListNode n3 = new ListNode(4);
+
+        n1.next = n2;
+        n2.next = n3;
+
+        ListNode listNode = addTwoNumbers(l1, n1);
+        System.out.println(listNode);
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(0);
+        ListNode cursor = root;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0) {
+            int l1Val = l1 != null ? l1.val : 0;
+            int l2Val = l2 != null ? l2.val : 0;
+            int sumVal = l1Val + l2Val + carry;
+            carry = sumVal / 10;
+
+            ListNode sumNode = new ListNode(sumVal % 10);
+            cursor.next = sumNode;
+            cursor = sumNode;
+
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }
+
+        return root.next;
+    }
+
+}

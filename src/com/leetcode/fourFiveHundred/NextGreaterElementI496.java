1244541fba5d2cf9a99d18d78dbebca9e4674302
wanhao <838088516@qq.com>
2020-01-14 10:28:08 +0800

    496 下一个更大元素 I

---
+++ src/com/leetcode/fourFiveHundred/NextGreaterElementI496.java
@@ -0,0 +1,53 @@
+package com.leetcode.fourFiveHundred;
+
+import java.util.Arrays;
+
+/**
+ * Created by LiuLiHao on 2020/1/14 0014 上午 09:57
+ * @author : LiuLiHao
+ * 描述：
+ * 给定两个没有重复元素的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。找到 nums1 中每个元素在 nums2 中的下一个比其大的值。
+ *
+ * nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出-1。
+ *
+ * 示例 1:
+ *
+ * 输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
+ * 输出: [-1,3,-1]
+ * 解释:
+ *     对于num1中的数字4，你无法在第二个数组中找到下一个更大的数字，因此输出 -1。
+ *     对于num1中的数字1，第二个数组中数字1右边的下一个较大数字是 3。
+ *     对于num1中的数字2，第二个数组中没有下一个更大的数字，因此输出 -1。
+ *
+ */
+public class NextGreaterElementI496 {
+
+    public static void main(String[] args) {
+        int[] nums1 = {4,1,2};
+        int[] nums2 = {1,3,4,2};
+
+        int[] ints = nextGreaterElement(nums1, nums2);
+        System.out.println(Arrays.toString(ints));
+    }
+
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            int cur = nums1[i];
+            int temp = -1;
+            boolean flag = false;
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums2[j]==cur){
+                    flag = true;
+                }
+                if (flag && nums2[j]>cur){
+                    temp = nums2[j];
+                    break;
+                }
+            }
+            result[i] = temp;
+        }
+        return result;
+    }
+}

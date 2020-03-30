c5fd69c9d3ab10bb06b76fd596c6f2bed940974e
liulihao <838088516@qq.com>
2020-03-30 15:20:23 +0800

    最小绝对差

---
+++ src/com/leetcode/twelveToThirteen/MinimumAbsoluteDifference1200.java
@@ -0,0 +1,70 @@
+package com.leetcode.twelveToThirteen;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by LiuLiHao on 2020/3/30 0030 下午 03:06
+ * @author : LiuLiHao
+ * 描述：
+ * 给你个整数数组 arr，其中每个元素都 不相同。
+ *
+ * 请你找到所有具有最小绝对差的元素对，并且按升序的顺序返回。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：arr = [4,2,1,3]
+ * 输出：[[1,2],[2,3],[3,4]]
+ * 示例 2：
+ *
+ * 输入：arr = [1,3,6,10,15]
+ * 输出：[[1,3]]
+ * 示例 3：
+ *
+ * 输入：arr = [3,8,-10,23,19,-4,-14,27]
+ * 输出：[[-14,-10],[19,23],[23,27]]
+ *  
+ *
+ * 提示：
+ *
+ * 2 <= arr.length <= 10^5
+ * -10^6 <= arr[i] <= 10^6
+ *
+ */
+public class MinimumAbsoluteDifference1200 {
+    public static void main(String[] args) {
+
+        int[] nums = {3,8,-10,23,19,-4,-14,27};
+        List<List<Integer>> lists = minimumAbsDifference(nums);
+        System.out.println(lists);
+
+    }
+
+    public static List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i] - arr[i - 1];
+            if (temp < min) {
+                min = temp;
+            }
+        }
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i] - arr[i - 1];
+            if (temp == min) {
+                List<Integer> cur = new ArrayList<>();
+                cur.add(arr[i - 1]);
+                cur.add(arr[i]);
+
+                result.add(cur);
+            }
+        }
+
+        return result;
+    }
+}
